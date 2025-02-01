3f38be51ce3c8f37975444b1a758888d051e7e63
Denis Tsariov <tsariov.den@gmail.com>
2025-02-01 11:59:55 -0500

    tabs

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,33 @@
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform } from "react-native";
+
+import { House } from "lucide-react-native";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        // tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: false,
+        // tabBarButton: HapticTab,
+        // tabBarBackground: TabBarBackground,
+        tabBarStyle: Platform.select({
+          ios: {
+            // Use a transparent background on iOS to show the blur effect
+            position: "absolute",
+          },
+          default: {},
+        }),
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "index page",
+          tabBarIcon: ({ color }) => <House color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
